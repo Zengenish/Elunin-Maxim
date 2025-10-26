00c9b627d9cb5e8c9c262df0399dea19d48bcc00
Zengenish <makspvp10@mail.ru>
2025-10-26 20:04:59 +0300

    Add files via upload

---
+++ Практическая 11/Задание 1/Задание 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    class Car
+    {
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 0)
+                {
+                    speed = 0;
+                }
+                else if (value > 300)
+                {
+                    Console.WriteLine("Скорость не может превышать 300!");
+                    speed = 300;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+
+        public string Model { get; set; } = "Unknown";
+
+        public Car(string model, int speed)
+        {
+            Model = model;
+            Speed = speed;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Model} едет со скоростью {Speed} км/ч.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Mercedes", 220);
+            myCar.Drive();
+
+            myCar.Speed = 400;
+            myCar.Drive();
+
+            myCar.Speed = -50;
+            myCar.Drive();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практическая 11/Задание 3/Задание 3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+
+    class Book
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Название не может быть пустым!");
+                }
+                else
+                {
+                    title = value;
+                }
+            }
+        }
+        public int Pages { get; set; } = 1;
+
+        public bool IsLong
+        {
+            get { return Pages > 300; }
+        }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Книга: {Title}, страниц: {Pages}, длинная: {(IsLong ? "да" : "нет")}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("Война и мир");
+            myBook.Pages = 1200;
+            myBook.Info();
+
+            Book emptyBook = new Book(""); 
+            emptyBook.Pages = 10;
+            emptyBook.Info();
+
+            Book shortBook = new Book("Краткая история");
+            shortBook.Pages = 1;
+            shortBook.Info();
+            Console.ReadKey();
+        }
+    }
+}
