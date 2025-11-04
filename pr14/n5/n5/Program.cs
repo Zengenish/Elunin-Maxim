28d6b1c4018fa23db687ac9a6ee10c11cb09c6cd
Zengenish <makspvp10@mail.ru>
2025-11-04 21:50:26 +0300

    Практическая 14

---
+++ pr14/n5/n5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Player
+{
+    public virtual void Attack()
+    {
+        Console.WriteLine("Атакую!");
+    }
+}
+
+class Warrior : Player
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Рублю мечом!");
+    }
+}
+
+class Archer : Player
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Стреляю из лука!");
+    }
+}
+
+class ProgramPlayers
+{
+    static void Main()
+    {
+        Player[] team = { new Warrior(), new Archer(), new Player() };
+        foreach (var p in team)
+        {
+            p.Attack();
+        }
+        Console.ReadLine();
+    }
+}
